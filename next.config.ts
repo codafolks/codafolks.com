767ac437c3665a51a5503fe3b4e949b314b0b43b
Falconiere R. Barbosa <falconieer@gmail.com>
2024-11-28 13:41:44 -0300

    Fix next.config and taiwind config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig:NextConfig = {
-  output: 'export',
+  output: "standalone",
+  serverExternalPackages: ["pino", "pino-pretty"],
+  experimental: {
+    optimizeCss: true,
+    optimisticClientCache: true,
+    optimizeServerReact: true,
+    nextScriptWorkers: true,
+    gzipSize: true,
+    staleTimes: {
+      dynamic: 60 * 60 * 24, // 24 hours
+      static: 60 * 60 * 24, // 24 hours
+    },
+  },
+  compiler: {
+    removeConsole: {
+      exclude: ["error", "info", "warn"],
+    },
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
