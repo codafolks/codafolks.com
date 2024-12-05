a1d0ebcb821b7f6886cbf830ca5a62ec333887b8
Falconiere R. Barbosa <falconieer@gmail.com>
2024-12-05 00:17:19 -0300

    Add home sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+		},
 	},
-	plugins: [],
+	plugins: [
+		function ({ addComponents, theme }) {
+			addComponents({
+				'.bg-grid': {
+					"background-image": "url('/assets/grid-100x100.png')",
+					"background-size": "100px 100px",
+					"background-position-y": "-9px",
+					"-webkit-mask-image": "linear-gradient(to bottom, transparent, 10%, white, 90%, transparent)",
+					"mask-image": "linear-gradient(to bottom, transparent, 10%, white, 90%, transparent)"
+				},
+			});
+		},
+	],
 }
